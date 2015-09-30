2e4526b2cf4079e32e3660f4f3f58fd44eb5a422
MichaelBiermann <michbier@gmail.com>
2015-09-30 15:56:28 +0000

    first module for post requests

--- webServerStart.js
+++ webServerStart.js
@@ -1,4 +1,5 @@
 // 'Hallo Welt' Webserver
+var posthandler = require('./js/posthandler');
 var express = require('express');
 var app = express();
 var http = require('http');
@@ -7,9 +8,12 @@ var port = 3000;
 app.use('/', express.static(__dirname));
 app.use('/js', express.static(__dirname));
 app.use('/css', express.static(__dirname));
+// app.use(express.bodyParser());
 app.get('/*', function(req, res) {
     res.status(404).sendFile(__dirname + '/error.html');
 });
+
+debugger;
 
 var server = http.createServer(app).listen(port,'0.0.0.0');
 console.log('Call the server via http://0.0.0.0:' + port + '/');
