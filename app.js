5a7c5a94d88f11bea45f287c12a46b7bd4eefc17
Michael Biermann <michbier@gmail.com>
2015-10-04 18:11:14 +0000

    Take process.env.OPENSHIFT... into account for port/ip

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // Webserver for geo tracker
 var getHandler = require('./lib/getHandler');
 var postHandler = require('./lib/postHandler');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
 var http = require('http');
-var port = 8081;    // Nitrous
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8081;   // 8081 -> Nitrous
 
 app.set('port', port);
 app.use('/', express.static(__dirname));
@@ -20,7 +21,7 @@ postHandler.registerHandlerGeodataPost(app);
 // debugger;
 
 // use host '0.0.0.0' to connect from any host
-var server = http.createServer(app).listen(app.get('port'), '0.0.0.0', function() {
+var server = http.createServer(app).listen(app.get('port'), ipaddress, function() {
         console.log('Listening on port %d', server.address().port);
         console.log('i.e. http://umkk1a021936.michaelbiermann.koding.io:'+server.address().port)
         console.log('i.e. http://geotracker-js-131771.nitrousapp.com:'+server.address().port)
