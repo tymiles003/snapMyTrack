3a790d57710be494d8c7d63d5fe02418ddfde638
MichaelBiermann <michbier@gmail.com>
2015-10-07 07:46:32 +0000

    Make use of dev. config ('.dev_config/config.json')

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Webserver for app 'geo tracker'
+var dev_config = require('./.dev_config/config.json');
 var getHandler = require('./lib/getHandler');
 var postHandler = require('./lib/postHandler');
 var http = require('http');
@@ -8,6 +9,7 @@ var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '0.0.0.0';
 // var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8081;   // def: 8081->Codenvy, 3000->Nitrous
 var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;   // def: 8081->Codenvy, 3000->Nitrous
+var mongodb_db_url;
 
 app.set('ip', ipaddress);
 app.set('port', port);
@@ -24,9 +26,15 @@ postHandler.registerHandlerGeodataPost(app);
 
 console.log('ip:'+ipaddress);
 console.log('port:'+port);
+
 // OPENSHIFT_MONGODB_DB_URL set via SSH (Chrome app 'Secure Shell'), see path .env/user_vars
 if(process.env.OPENSHIFT_MONGODB_DB_URL){
-  console.log('mongodb Url (process.env.OPENSHIFT_MONGODB_DB_URL): ', process.env.OPENSHIFT_MONGODB_DB_URL);   // deactivate later
+  mongodb_db_url=process.env.OPENSHIFT_MONGODB_DB_URL;
+  console.log('mongodb Url (process.env.OPENSHIFT_MONGODB_DB_URL): ', process.env.OPENSHIFT_MONGODB_DB_URL);   // ToDo: May be deactivate later
+}
+else if(dev_config.MONGODB_DB_URL){
+  mongodb_db_url=dev_config.MONGODB_DB_URL;
+  console.log('mongodb Url (dev_config.MONGODB_DB_URL): ', dev_config.MONGODB_DB_URL);   // ToDo: May be deactivate later
 }
 else{
   console.log('mongodbUrl: process.env.OPENSHIFT_MONGODB_DB_URL not available');  // deactivate later
