efb3f44eb3b30453f29d9646071168400129afab
MichaelBiermann <michbier@gmail.com>
2015-10-08 21:19:04 +0000

    user setting, path label, new files

---
+++ public/js/settings.js
@@ -0,0 +1,33 @@
+var userSettings = {
+    userId: "",
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+//      mapTypeId: google.maps.MapTypeId.ROADMAP,
+//      mapTypeId: google.maps.MapTypeId.SATELLITE,
+//      mapTypeId: google.maps.MapTypeId.HYBRID,
+//      mapTypeId: google.maps.MapTypeId.TERRAIN,
+
+function toggleSettings(evt){
+    if($('#settingsPopin').hasClass('isInvisible')){
+        $('#settingsPopin').removeClass('isInvisible');
+    }
+    else{
+        $('#settingsPopin').addClass('isInvisible');
+        if( $('#mapTypeSelect').val() !== userSettings.mapTypeId ){
+            userSettings.mapTypeId = $('#mapTypeSelect').val();
+            changeMapType( userSettings.mapTypeId );
+            var userId = $("#userId").val();
+            if(!userId){
+                userId = 'allUsers';
+            }
+            sendUserSettingsToServer( userId, userSettings.mapTypeId ); 
+        }
+    }
+}
+
+function updateUserSettings(userSettingsNew){
+    if( userSettingsNew.mapTypeId !== userSettings.mapTypeId ){
+        userSettings.mapTypeId = userSettingsNew.mapTypeId;
+        changeMapType( userSettings.mapTypeId );
+    }
+}
